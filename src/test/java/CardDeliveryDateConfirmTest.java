39247f32fd9f999efce13f49c8e4d53df0805808
Taisia Ivanovskaya <taya.naumova@yahoo.com>
2023-03-22 10:57:21 +0300

    Test corrected

--- src/test/java/CardDeliveryDateConfirmTest.java
+++ src/test/java/CardDeliveryDateConfirmTest.java
@@ -14,8 +14,6 @@ import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryDateConfirmTest {
@@ -49,9 +47,9 @@ public class CardDeliveryDateConfirmTest {
         form.$("[data-test-id=phone] input").setValue("+7" + registrationInfo.getPhoneNumber());
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-//        $(".notification__content")
-//                .shouldHave(Condition.text("Встреча успешно забронирована на " + formatter.format(registrationInfo.getFirstMeetingDate())), Duration.ofSeconds(15))
-//                .shouldBe(Condition.visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + formatter.format(registrationInfo.getFirstMeetingDate())), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         form.$("[data-test-id=date] input").setValue(formatter.format(registrationInfo.getSecondMeetingDate()));
